adf9f59acbc19025c7f5706f22739fa70f46cde8
Mike Hatfield <oakraven13@gmail.com>
2014-11-11 14:50:34 -0400

    Refactored site structure

--- server.js
+++ server.js
@@ -1,55 +1,15 @@
-var express = require('express'),
-    stylus = require('stylus'),
-    logger = require('morgan'),
-    bodyParser = require('body-parser'),
-    mongoose = require('mongoose');
+var express = require('express')
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
+var config = require('./server/config/config')[env];
 
-var compile = function(str, path) {
-  return stylus(str).set('filename', path);
-}
+require('./server/config/express')(app, config);
 
-app.set('views', __dirname + '/server/views');
-app.set('view engine', 'jade');
-app.use(logger('dev'));
-app.use(bodyParser());
-app.use(stylus.middleware({
-  src: __dirname + '/public',
-  compile: compile
-}));
-app.use(express.static(__dirname + '/public'));
+require('./server/config/mongoose')(config);
 
-if(env === 'development'){
-  mongoose.connect('mongodb://localhost/multivision');
-} else {
-  mongoose.connect('mongodb://mhatfield:multivision@ds051980.mongolab.com:51980/multivision');
-}
+require('./server/config/routes')(app);
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error...'));
-db.once('open', function callback(){
-  console.log('multivision database opened');
-});
-var messageSchema = mongoose.Schema({message: String});
-var Message = mongoose.model('Message', messageSchema);
-var mongoMessage;
-Message.findOne().exec(function(err, messageDoc) {
-  mongoMessage = messageDoc.message;
-});
-
-app.get('/partials/:partialPath', function(req, res) {
-  res.render('partials/' + req.params.partialPath);
-})
-
-app.get('*', function(req, res) {
-  res.render('index', {
-    mongoMessage: mongoMessage
-  });
-});
-
-var port = process.env.PORT || 3030;
-app.listen(port);
-console.log('Listening on port ' + port + '...');
+app.listen(config.port);
+console.log('Listening on port ' + config.port + '...');
